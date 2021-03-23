30a269c551b1caba5392a9c5d6a33e137ff2e63e
Camila Alves Meyer <camila.alves85@gmail.com>
2021-03-23 13:26:39 -0700

    Cleaned unused code

--- server/db/DeleteTable.js
+++ server/db/DeleteTable.js
@@ -16,19 +16,4 @@ dynamodb.deleteTable(params, function (err, data) {
   if (err) console.log(err, err.stack);
   // an error occurred
   else console.log(data); // successful response
-  /*
-    data = {
-     TableDescription: {
-      ItemCount: 0, 
-      ProvisionedThroughput: {
-       NumberOfDecreasesToday: 1, 
-       ReadCapacityUnits: 5, 
-       WriteCapacityUnits: 5
-      }, 
-      TableName: "Music", 
-      TableSizeBytes: 0, 
-      TableStatus: "DELETING"
-     }
-    }
-    */
 });
